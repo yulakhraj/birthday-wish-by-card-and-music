27951b7b6a5cb01432e5b2edaff7e49b6f4e4934
MANISH RAJ <47447422+yulakhraj@users.noreply.github.com>
2021-07-04 13:15:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var a=prompt("Enter your name");
+var b=prompt("Enter your age");
+alert("Welcome "+a+ " ! "+"You are "+ b + " years old. Click ok to celebrate your birthday")
+
+window.location.href="start.html";
